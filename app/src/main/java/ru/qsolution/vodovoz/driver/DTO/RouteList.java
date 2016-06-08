b1f2e234397e95b1805a3fe159a740f0eb50abb5
Andrei <anri.vin@gmail.com>
2016-06-08 18:05:09 +0300

    Added route list DTO and soap request parsing to it.

---
+++ app/src/main/java/ru/qsolution/vodovoz/driver/DTO/RouteList.java
@@ -0,0 +1,33 @@
+package ru.qsolution.vodovoz.driver.DTO;
+
+import org.ksoap2.serialization.SoapObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Andrei on 08.06.16.
+ */
+public class RouteList {
+    public String Id;
+    public String Status;
+    public String Forwarder;
+    public String DeliveryShift;
+    public Date Date;
+
+    public RouteList (SoapObject soapObject) {
+        Id = soapObject.getProperty("Id").toString();
+        Status = soapObject.getProperty("Status").toString();
+        Forwarder = soapObject.getProperty("Forwarder").toString();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        try {
+            Date = sdf.parse(soapObject.getProperty("Date").toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Date = new Date();
+        }
+        DeliveryShift = soapObject.getProperty("DeliveryShift").toString();
+    }
+
+}
--- app/src/main/java/ru/qsolution/vodovoz/driver/RouteListsActivity.java
+++ app/src/main/java/ru/qsolution/vodovoz/driver/RouteListsActivity.java
@@ -5,10 +5,16 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.ksoap2.serialization.SoapObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 import ru.qsolution.vodovoz.driver.AsyncTasks.CheckAuthTask;
 import ru.qsolution.vodovoz.driver.AsyncTasks.GetRouteListsTask;
+import ru.qsolution.vodovoz.driver.DTO.RouteList;
 
 public class RouteListsActivity extends AppCompatActivity {
 
@@ -21,9 +27,16 @@ public class RouteListsActivity extends AppCompatActivity {
         SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.auth_file_key), Context.MODE_PRIVATE);
 
         try {
-            Object routeLists = new GetRouteListsTask().execute(sharedPref.getString("Authkey", "")).get();
-            if (true) {
-                String asd = "asd";
+            Object routeListsObj = new GetRouteListsTask().execute(sharedPref.getString("Authkey", "")).get();
+            if (routeListsObj instanceof SoapObject) {
+                SoapObject routeLists = (SoapObject) routeListsObj;
+                for (int i = 0; i < routeLists.getPropertyCount(); i++) {
+                    Object property = routeLists.getProperty(i);
+                    if (property instanceof SoapObject) {
+                        SoapObject soapObject = (SoapObject) property;
+                        RouteList routeList = new RouteList(soapObject);
+                    }
+                }
             }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
