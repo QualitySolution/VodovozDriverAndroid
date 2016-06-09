dfe27061aacdb62b56bc6e810dcdea0865646e81
Andrei <anri.vin@gmail.com>
2016-06-09 21:06:01 +0300

    Fixed retriving Order from SOAP.

--- app/src/main/java/ru/qsolution/vodovoz/driver/DTO/Order.java
+++ app/src/main/java/ru/qsolution/vodovoz/driver/DTO/Order.java
@@ -2,6 +2,8 @@ package ru.qsolution.vodovoz.driver.DTO;
 
 import org.ksoap2.serialization.SoapObject;
 
+import ru.qsolution.vodovoz.driver.Workers.SoapWorker;
+
 /**
  * Created by Andrei on 08.06.16.
  */
@@ -34,31 +36,31 @@ public class Order {
     public Float Longitude;
 
     public Order (SoapObject soapObject) {
-        Id = soapObject.getProperty("Id").toString();
-        Title = soapObject.getProperty("Title").toString();
-        LocalityType = soapObject.getProperty("LocalityType").toString();
-        City = soapObject.getProperty("City").toString();
-        Housing = soapObject.getProperty("Housing").toString();
-        Letter = soapObject.getProperty("Letter").toString();
-        Structure = soapObject.getProperty("Structure").toString();
-        Placement = soapObject.getProperty("Placement").toString();
-        Floor = soapObject.getProperty("Floor").toString();
-        Region = soapObject.getProperty("Region").toString();
-        Street = soapObject.getProperty("Street").toString();
-        CityDistrict = soapObject.getProperty("CityDistrict").toString();
-        StreetDistrict = soapObject.getProperty("StreetDistrict").toString();
-        Building = soapObject.getProperty("Building").toString();
-        RoomType = soapObject.getProperty("RoomType").toString();
-        Room = soapObject.getProperty("Room").toString();
-        DeliveryPointComment = soapObject.getProperty("DeliveryPointComment").toString();
-        Contact = soapObject.getProperty("Contact").toString();
-        Phone = soapObject.getProperty("Phone").toString();
-        DeliverySchedule = soapObject.getProperty("DeliverySchedule").toString();
-        OrderStatus = soapObject.getProperty("OrderStatus").toString();
-        OrderComment = soapObject.getProperty("OrderComment").toString();
-        Counterparty = soapObject.getProperty("Counterparty").toString();
+        Id = SoapWorker.SafeGetPropertyAsString(soapObject, "Id");
+        Title = SoapWorker.SafeGetPropertyAsString(soapObject, "Title");
+        LocalityType = SoapWorker.SafeGetPropertyAsString(soapObject, "LocalityType");
+        City = SoapWorker.SafeGetPropertyAsString(soapObject, "City");
+        Housing = SoapWorker.SafeGetPropertyAsString(soapObject, "Housing");
+        Letter = SoapWorker.SafeGetPropertyAsString(soapObject, "Letter");
+        Structure = SoapWorker.SafeGetPropertyAsString(soapObject, "Structure");
+        Placement = SoapWorker.SafeGetPropertyAsString(soapObject, "Placement");
+        Floor = SoapWorker.SafeGetPropertyAsString(soapObject, "Floor");
+        Region = SoapWorker.SafeGetPropertyAsString(soapObject, "Region");
+        Street = SoapWorker.SafeGetPropertyAsString(soapObject, "Street");
+        CityDistrict = SoapWorker.SafeGetPropertyAsString(soapObject, "CityDistrict");
+        StreetDistrict = SoapWorker.SafeGetPropertyAsString(soapObject, "StreetDistrict");
+        Building = SoapWorker.SafeGetPropertyAsString(soapObject, "Building");
+        RoomType = SoapWorker.SafeGetPropertyAsString(soapObject, "RoomType");
+        Room = SoapWorker.SafeGetPropertyAsString(soapObject, "Room");
+        DeliveryPointComment = SoapWorker.SafeGetPropertyAsString(soapObject, "DeliveryPointComment");
+        Contact = SoapWorker.SafeGetPropertyAsString(soapObject, "Contact");
+        Phone = SoapWorker.SafeGetPropertyAsString(soapObject, "Phone");
+        DeliverySchedule = SoapWorker.SafeGetPropertyAsString(soapObject, "DeliverySchedule");
+        OrderStatus = SoapWorker.SafeGetPropertyAsString(soapObject, "OrderStatus");
+        OrderComment = SoapWorker.SafeGetPropertyAsString(soapObject, "OrderComment");
+        Counterparty = SoapWorker.SafeGetPropertyAsString(soapObject, "Counterparty");
         RouteListItemStatus = "FIX ME";
-        Latitude = Float.parseFloat(soapObject.getProperty("CityDistrict").toString());
-        Longitude = Float.parseFloat(soapObject.getProperty("CityDistrict").toString());
+        Latitude = Float.parseFloat(SoapWorker.SafeGetPropertyAsString(soapObject, "CityDistrict"));
+        Longitude = Float.parseFloat(SoapWorker.SafeGetPropertyAsString(soapObject, "CityDistrict"));
     }
 }
---
+++ app/src/main/java/ru/qsolution/vodovoz/driver/Workers/SoapWorker.java
@@ -0,0 +1,23 @@
+package ru.qsolution.vodovoz.driver.Workers;
+
+import org.ksoap2.serialization.SoapObject;
+
+/**
+ * Created by Andrei on 09.06.16.
+ */
+public class SoapWorker {
+    private static final String EMPTY_STRING = "";
+
+    public static String SafeGetPropertyAsString (SoapObject soapObject, String propertyName) {
+        try {
+            Object property = soapObject.getProperty(propertyName);
+            if (property == null)
+                return EMPTY_STRING;
+            else
+                return property.toString();
+        } catch (RuntimeException e) {
+            return EMPTY_STRING;
+        }
+
+    }
+}
