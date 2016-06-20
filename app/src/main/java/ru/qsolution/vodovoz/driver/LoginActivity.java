c7680813ca88b7e1986188b9718caeac4de9253d
Andrei <anri.vin@gmail.com>
2016-06-20 16:40:00 +0300

    Added version checking

--- app/src/main/java/ru/qsolution/vodovoz/driver/LoginActivity.java
+++ app/src/main/java/ru/qsolution/vodovoz/driver/LoginActivity.java
@@ -1,11 +1,20 @@
 package ru.qsolution.vodovoz.driver;
 
+import android.app.DownloadManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +23,8 @@ import android.widget.Toast;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.concurrent.ExecutionException;
+
 import ru.qsolution.vodovoz.driver.AsyncTasks.*;
 
 /**
@@ -21,17 +32,30 @@ import ru.qsolution.vodovoz.driver.AsyncTasks.*;
  * (c) Quality Solution Ltd.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements IAsyncTaskListener<AsyncTaskResult<Boolean>> {
     private EditText usernameInput;
     private EditText passwordInput;
+    private Button loginButton;
+    private SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        usernameInput = (EditText) findViewById(R.id.loginInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
 
         Context context = this.getApplicationContext();
-        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.auth_file_key), Context.MODE_PRIVATE);
+        sharedPref = context.getSharedPreferences(getString(R.string.auth_file_key), Context.MODE_PRIVATE);
         Bundle extras = getIntent().getExtras();
+
+        //Checking app API version
+        CheckAppVersionTask checkAppVersionTask = new CheckAppVersionTask();
+        checkAppVersionTask.addListener(this);
+        checkAppVersionTask.execute(BuildConfig.VERSION_CODE);
 
         //Checking if needed to close app
         if (extras != null) {
@@ -40,22 +64,9 @@ public class LoginActivity extends AppCompatActivity {
                 return;
             }
         }
-        //Checking authorization
-        if (sharedPref.contains("Authkey")) {
-            CheckAuthListener listener = new CheckAuthListener(this, sharedPref);
-            CheckAuthTask task = new CheckAuthTask();
-            task.addListener(listener);
-            task.execute(sharedPref.getString("Authkey", ""));
-        }
-
-        setContentView(R.layout.activity_login);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        usernameInput = (EditText) findViewById(R.id.loginInput);
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
 
         //Authorization logic
+        loginButton.setEnabled(false);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,6 +82,81 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        usernameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (TextUtils.isEmpty(s) || TextUtils.isEmpty(passwordInput.getText())) {
+                    loginButton.setEnabled(false);
+                } else {
+                    loginButton.setEnabled(true);
+                }
+
+            }
+        });
+
+        passwordInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (TextUtils.isEmpty(s) || TextUtils.isEmpty(usernameInput.getText())) {
+                    loginButton.setEnabled(false);
+                } else {
+                    loginButton.setEnabled(true);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void AsyncTaskCompleted(AsyncTaskResult<Boolean> result) {
+        if (result.getException() == null && !result.getResult()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Требуется обновление")
+                    .setMessage("Используется устаревшая версия программы. " +
+                            "Для продолжения требуется скачать обновленное приложение.")
+                    .setCancelable(false)
+                    .setNegativeButton("Выход", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            LoginActivity.this.finish();
+                        }
+                    })
+                    .setPositiveButton("Скачать", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            LoginActivity.this.finish();
+                            String url = "http://files.qsolution.ru/Vodovoz/VodovozDrivers.apk";
+                            Intent i = new Intent(Intent.ACTION_VIEW);
+                            i.setData(Uri.parse(url));
+                            startActivity(i);
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        } else {
+            //Checking authorization
+            if (sharedPref.contains("Authkey")) {
+                CheckAuthListener listener = new CheckAuthListener(this, sharedPref);
+                CheckAuthTask task = new CheckAuthTask();
+                task.addListener(listener);
+                task.execute(sharedPref.getString("Authkey", ""));
+            }
+        }
     }
 
     private class LoginListener implements IAsyncTaskListener<AsyncTaskResult<String>> {
