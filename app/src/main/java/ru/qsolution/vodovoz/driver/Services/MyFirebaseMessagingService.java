5f1ef96570fba611f435ddb3170baa03f50f21d2
Andrei <anri.vin@gmail.com>
2016-07-11 09:33:12 +0300

    Additional messenging abilities

--- app/src/main/java/ru/qsolution/vodovoz/driver/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/ru/qsolution/vodovoz/driver/Services/MyFirebaseMessagingService.java
@@ -3,12 +3,8 @@ package ru.qsolution.vodovoz.driver.Services;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
